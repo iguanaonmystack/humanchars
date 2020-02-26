def7fdcfc8dd63fb23824ae4429ebd11e183ac73
Kathy Murdoch <kathy@nivan.net>
2020-02-26 20:03:34 +0000

    Initial commit.

---
+++ humanchars.py
@@ -0,0 +1,27 @@
+import unicodedata
+
+def split(s):
+    s = unicodedata.normalize('NFC', s)
+    _buffer = []
+    for char in s:
+        if unicodedata.combining(char):
+            _buffer.append(char)
+            continue
+        if _buffer:
+            yield ''.join(_buffer)
+        _buffer = [char]
+    if _buffer:
+        yield ''.join(_buffer)
+
+
+def debugprint(l):
+    for chars in l:
+        print(chars,
+              ' '.join('%04x' % ord(c) for c in chars))
+    print()
+
+
+debugprint(split('c\u0061\u0301t'))
+debugprint(split('c\u0061\u0305t'))
+debugprint(split('᚛ᚑᚌᚐᚋ᚜'))
+debugprint(split('اَلْعَرَبِيَّةُ'))
